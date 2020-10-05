a04466cd306df57bf1b7509910598fd46d993908
wlgud0402 <wlgudrlgus@naver.com>
2020-10-05 16:34:50 +0900

    use mysql

---
+++ config.py
@@ -0,0 +1,9 @@
+db = {
+    'user': 'root',
+    'password': 'root'
+    'host': 'localhost',
+    'port': 5000,
+    'database': 'miniter'
+}
+
+DB_URL = f"mysql + mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8"
